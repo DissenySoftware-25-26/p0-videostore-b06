725f4bb7f9319b078e970b0331aa2b6d769f18b6
MRL2005 <mikirodrilopez@gmail.com>
2025-09-27 20:44:41 +0200

    Prueba

--- src/Customer.java
+++ src/Customer.java
@@ -21,6 +21,7 @@ public class Customer
 	public String statement () {
 		double 				totalAmount 			= 0;
 		int					frequentRenterPoints 	= 0;
+        int uno=1;
 		String result 		= "Rental Record for " + getName() + "\n";
 
         for (Rental rental : this.rentals) {
