c2aad18d939a75df31c4b1768c5b80f041e6a4eb
Fructa15 <mmgpupi@gmail.com>
2025-09-27 20:02:53 +0200

    Hello

--- src/Rental.java
+++ src/Rental.java
@@ -6,7 +6,29 @@ public class Rental
 		this.daysRented = daysRented;
 	}
 
-	public int getDaysRented () {
+    double getRentalPrice () {
+        double price = 0;
+
+        // determines the amount for each line
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                price += 2;
+                if (this.getDaysRented() > 2)
+                    price += (this.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                price += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                price += 1.5;
+                if (this.getDaysRented() > 3)
+                    price += (this.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return price;
+    }
+
+    public int getDaysRented () {
 		return daysRented;
 	}
 
@@ -14,6 +36,7 @@ public class Rental
 		return movie;
 	}
 
-	private Movie movie;
-	private int daysRented;
+	private final Movie movie;
+	private final int daysRented;
+
 }
